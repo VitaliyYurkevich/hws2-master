45cbcac4ca285466c97190a1339a96421719a446
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-18 14:45:44 +0300

    DZ 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,33 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+const SET_THEME_ID = 'SET_THEME_ID'
+
+type changeThemeId = {
+    type: typeof SET_THEME_ID
+    id: number
+}
+
+type ThemeType = typeof initState
+
+export const themeReducer = (state = initState, action: changeThemeId): ThemeType => { // fix any
     switch (action.type) {
         // дописать
-
+            case 'SET_THEME_ID': {
+                return {
+                    ...state,
+                    themeId: action.id
+                }
+            }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type changeThemeIdType = {
+    type: string,
+    id: number
+}
+
+
+export const changeThemeId = (id: number): changeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
