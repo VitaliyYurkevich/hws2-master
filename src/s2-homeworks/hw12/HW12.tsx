a0dc092bd1db7b6a53ff65e03ce99dca9804a6bc
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-18 15:14:02 +0300

    DZ 12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -4,7 +4,7 @@ import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
 import {changeThemeId} from './bll/themeReducer'
-import {AppStoreType} from "../hw10/bll/store";
+import  {AppStoreType} from "../hw10/bll/store";
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
